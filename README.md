45255b82ab4144d02f115fee853bb1f1a0fb5ece
wistyuan <45817024+wistyuan@users.noreply.github.com>
2018-12-13 21:19:26 +0800

    commit
    commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# resume
+## 黄志远个人简历
+
+原版动态效果参见 http://strml.net/和https://shiningdan.github.io/react-animation-resume/
+
+谢谢两位大神
+
+### 使用方法
+
+在线的项目展示效果可以点击链接： [React Animation Resume](https://wistyuan.github.io/resume/)
+
+如果想在本地下载、编译、运行，可以参考以下命令：
+
+```
+git clone https://github.com/wistyuan/resume.git
+cd resume/
+npm install
+npm run build
+npm run start
+```
+
+就可以在 http://localhost:8080/index.html 上看到项目的展示效果啦
---
+++ app/app.js
@@ -0,0 +1,260 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import StyleEditor from "./StyleEditor.js";
+import ResumeEditor from "./ResumeEditor.js";
+import "./style/reset.css";
+import Prism from "prismjs";
+
+class ReactClass extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			style: "",
+		};
+		this.interval = 40;
+		this.resumeEditorContent = `
+# 黄志远
+
+
+## 项目及工作经历
+
+### 软件定义云网 (Software-defined Cloud Network)
+
+* 基于 NSO 控制器开发 IDC 与运营商网络协同自动化框架。
+* 开发基于 Segment Routing 的广域网专线 QoS 调度系统。
+
+### 实验室项目：面向远程医疗的SDN网络可靠性及安全研究 (Java)
+
+* 基于 SDN 为数据中心远程医疗平台提供防火墙、DDoS 防御、QoS 保障、链路快速恢复、路由环路与黑洞检测能力。
+
+### [React Gallery](https://shiningdan.github.io/react-gallery/) (点击链接试试吧)
+
+* React 和 Webpack 制作的画廊应用效果
+
+### [Animation Resume](https://wistyuan.github.io/resume/) (点击链接试试吧)
+
+* React 和 Webpack 制作的简历自动演示效果。
+
+
+## 教育经历
+  
+1. **广 西 大 学  - 国际贸易**             本科
+
+	* 校一等奖学金
+
+2. **广西培贤学院 - 国际商务**             大专
+
+	* 连续三年国家励志奖学金
+
+## 其他
+
+CET-4 
+
+全国计算机二级
+
+	终于写完了，以上信息都是我吹的哈哈，开心就好 :)
+
+## 个人博客网站
+
+**GitHub: **https://github.com/wistyuan
+**Animation Resume: https://wistyuan.github.io/resume/**
+
+> 如果你喜欢这个效果，Fork [我的项目](https://github.com/wistyuan/resume)，或者可以找我要呀~~`;
+		
+		this.styleEditorContent = [`/*
+* Inspired by http://strml.net/ and https://github.com/ShiningDan/react-animation-resume
+* 
+* 你好, 我是黄志远
+* 8102年准备结束了，好多公司都加大力度开展新一轮的招聘工作，相信你也在为心仪的公司准备简历吧
+*
+* 下面我用 React 做了一份简易的动态简历
+* 希望大家能够喜欢 :)
+*/
+
+/* 所以我们就开始吧！首先给所有元素加上过渡效果 */
+* {
+  -webkit-transition: all 1s;
+  transition: all 1s;
+}
+/* 白色背景太单调了，我们来点背景 */
+html {
+  color: rgb(222,222,222); background: #425261; 
+}
+/* 文字直接显示在页面上，没有任何装饰，有点不舒服？那我们来给文字加点装饰吧~~ */
+.styleEditor {
+  position: fixed; left: 0; top: 0;
+  background-color: #303030;
+  padding: .5em;
+  border: 1px solid;
+  margin: .5em;
+  overflow: auto;
+  width: 45vw; height: 90vh;
+}
+/* 接下来给自己的代码加一点色彩吧 */
+.token.comment{ color: #857F6B; font-style: italic; }
+.token.selector{ color: #E86E75; }
+.token.property{ color: #F78C6C; }
+.token.punctuation{ color: #88DCFE; }
+.token.function{ color: #82AAFF; }
+
+/* 加一点 3D 效果，更加地酷炫 */
+html{
+  -webkit-perspective: 1000px;
+          perspective: 1000px;
+}
+.styleEditor {
+  position: fixed; left: 0; top: 0; 
+  -webkit-transition: none; 
+  transition: none;
+  -webkit-transform: rotateY(10deg) translateZ(-100px) ;
+          transform: rotateY(10deg) translateZ(-100px) ;
+}
+/* 一番折腾后终于把代码框修改好了 */
+
+/* 接下来我给自己准备一个编辑器，用来存放我的简历内容 */
+.resumeEditor{
+  position: fixed; right: 0; top: 0;
+  padding: .5em;  margin: .5em;
+  width: 48vw; height: 90vh; 
+  border: 1px solid;
+  background: white; color: #222;
+  overflow: auto;
+}
+
+/* 好了，我开始写简历了 */
+`,
+`
+/* 这个简历好像差点什么
+ * 对了，这是 Markdown 格式的，我需要变成对 HR 更友好的格式
+ * 简单，用开源工具翻译成 HTML 就行了
+ */
+`,
+`
+/* 再对 HTML 加点样式 */
+.resumeEditor{
+  padding: 2em;
+}
+.resumeEditor h1{
+  display: block;
+  width: 80px;
+  margin: 0 auto;
+}
+.resumeEditor h2{
+  display: inline-block;
+  border-bottom: 1px solid;
+  margin: 1em 0 .5em;
+}
+.resumeEditor h3{
+	display: inline-block;
+	margin: 0.5em 0;
+}
+.resumeEditor a{
+	color: #000;
+}
+.resumeEditor ul{
+	list-style: none;
+}
+.resumeEditor ul>li::before {
+	content: "•";
+	margin-left: 1em;
+	margin-right: 0.5em;
+}
+.resumeEditor blockquote {
+  margin: 1em;
+  padding: .5em;
+  background: #ddd;
+}
+/*
+* I hope you enjoyed this.
+*/
+`];
+	}
+
+	addToStyle(char) {
+		this.setState({
+			style: this.state.style + char,
+		});
+	}
+
+	replaceStyle(style) {
+		this.setState({
+			style: style,
+		});
+	}
+
+	replaceStyleEditorContent() {
+		
+	}
+
+	showStyleEditorContent(n) {
+		let lastContentLength = 0;
+		if (n !== 0) {lastContentLength = this.state.style.length;}
+		let style = this.styleEditorContent[n];
+		let len = style.length;
+		return new Promise((resolve, reject) => {
+			let showStyle = function () {
+				let currentLen = this.state.style.length - lastContentLength;
+				if (currentLen < len) {
+					let char = style.substring(currentLen, currentLen+1);
+					this.refs.StyleEditor.addToContent(char);
+					this.addToStyle(char);
+					setTimeout(showStyle, this.interval);
+				} else {
+					resolve();
+				}
+			}.bind(this);
+			showStyle();
+		});
+	}
+
+	showResumeContent() {
+		let content = this.resumeEditorContent;
+		let len = content.length;
+		return new Promise((resolve, reject) => {
+			let showContent = function() {
+				let currentLen = this.refs.ResumeEditor.getCurrentContentLength();
+				if (currentLen < len) {
+					let char = content.substring(currentLen, currentLen+1);
+					this.refs.ResumeEditor.addToContent(char);
+					setTimeout(showContent, this.interval);
+				} else {
+					resolve();
+				}
+			}.bind(this);
+			showContent();
+		});
+	}
+
+	setResumeMarkdown() {
+		return new Promise((resolve, reject) => {
+			setTimeout(this.refs.ResumeEditor.setIsMarkdown(true), this.interval);
+			resolve();
+		}); 
+	}
+
+	async startShow() {
+		await this.showStyleEditorContent(0).then(function() {console.log('done! show Content 0')});
+		await this.showResumeContent();
+		await this.showStyleEditorContent(1).then(function() {console.log('done! show Content 1')});
+		await this.setResumeMarkdown();
+		await this.showStyleEditorContent(2).then(function() {console.log('done! show Content 2')});
+	}
+
+	componentDidMount() {
+		this.startShow();
+		console.log(111);
+		// this.refs.StyleEditor.replaceContent(this.content[0]);
+		// this.replaceStyle(this.content[0]); 
+		// this.refs.ResumeEditor.replaceContent("");
+	}
+
+	render() {
+		return (
+			<div>
+				<StyleEditor ref="StyleEditor" />
+				<ResumeEditor ref="ResumeEditor" />
+				<style>{this.state.style}</style>
+			</div>);
+	}
+}
+ReactDOM.render(<ReactClass />, document.getElementById("content"));
